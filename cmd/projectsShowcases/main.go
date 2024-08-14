e7eddbaec0f249a95db10aa1c0a25e9381dcb71d
les <adkajsdasduoisaudkksak@dsfalasjkdlajsdk.ru>
2024-08-14 19:02:42 +0300

    Add logger

--- cmd/projectsShowcases/main.go
+++ cmd/projectsShowcases/main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"projectsShowcase/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env))
+
+	log.Info("initializing server", slog.String("address", cfg.Address))
+	log.Debug("logger debug mode enabled")
+}
+
+// setupLogger returns a logger based on the environment.
+//
+// The function takes an environment string as input and returns a pointer to a slog.Logger.
+// The logger is configured based on the environment:
+//
+// - If the environment is envLocal, a text handler with debug level is used.
+//
+// - If the environment is envDev, a JSON handler with debug level is used.
+//
+// - If the environment is envProd, a JSON handler with info level is used.
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
